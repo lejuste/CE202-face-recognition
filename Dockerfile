c397fceeb1788f11067132a4266c6bb6301e3d2f
Justin Lee <jus.lee18@gmail.com>
2018-03-05 21:03:21 -0800

    plot trial

--- Dockerfile
+++ Dockerfile
@@ -25,6 +25,10 @@ RUN apt-get install -y --fix-missing \
     python3-numpy \
     software-properties-common \
     zip \
+    parallel \
+    linux-tools-common \
+    linux-tools-generic \ 
+    linux-tools-4.13.0-36-generic \
     && apt-get clean && rm -rf /tmp/* /var/tmp/*
 
 RUN cd ~ && \
@@ -39,14 +43,14 @@ RUN cd ~ && \
 # If you wanted to use this Dockerfile to run your own app instead, maybe you would do this:
 # COPY . /root/your_app_or_whatever
 # RUN cd /root/your_app_or_whatever && \
-#     pip3 install -r requirements.txt
+#     pip install -r requirements.txt
 # RUN whatever_command_you_run_to_start_your_app
 
 COPY . /root/face_recognition
 RUN cd /root/face_recognition && \
-    pip3 install -r requirements.txt && \
+    pip install -r requirements.txt && \
     python3 setup.py install
 
 CMD cd /root/face_recognition/examples && \
-    python3 recognize_faces_in_pictures.py
+    python3 runTest.py
 
--- plotTest.py
+++ plotTest.py
@@ -1,13 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([0,3900000000,15625000000,62500000000,250000000000,1010000000000])
-y = np.array([2.4,2.2,2.0,1.8,1.6,1.4])
-my_xticks = ['0','~X/256','~X/64','~X/16','~X/4','~X']
-plt.xticks(x, my_xticks)
+from optparse import OptionParser
+import os
+
+parser = OptionParser()
+parser.add_option("-t", "--title", dest="title",
+                  help="Plot title", default='NO TITLE')
+parser.add_option("-d", "--data", dest="data",
+                  help="Input data as a string form of a list", default='1')
+
+(options, args) = parser.parse_args()
+
+title = str(options.title)
+list_string = str(options.data)
+
+print title
+print list_string
+
+list_list = list_string.split(",")
+print list_list
+
+
+
+
+x = np.array([3900000000,15625000000,62500000000,250000000000,1010000000000])
+y = np.array([2.2,2.0,1.8,1.6,1.4])
+my_xticks = ['~X/256','~X/64','~X/16','~X/4','~X']
+plt.xticks(x, my_xticks,rotation='90',fontsize = 5)
 plt.plot(x, y,'bo')
 plt.xlabel('Input Size')
 plt.ylabel('Instructions per cycle')
-plt.title('Thread 1 IPC')
+plt.title(str(title))#'Thread 1 IPC'
+plt.margins(0.1)
+plt.grid(True)
 plt.show()
-
